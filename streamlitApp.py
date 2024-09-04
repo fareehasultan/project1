25b924b2d838fe505c7fe14dbc2a1d4be5227730
fareehasultan <fareehasultan99@gmail.com>
2024-09-04 12:30:09 +0000

    added

--- streamlitApp.py
+++ streamlitApp.py
@@ -3,10 +3,11 @@ import joblib
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-model = joblib.load("livemodelv1.pk1")
+model = joblib.load("livemodelv1.pkl")
 
 data = pd.read_csv('mobile_price_range_data.csv')
-x = data.iloc[:,;]
+x = data.iloc[:,:-1]
+y = data.iloc[:, -1]             ]
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
@@ -20,7 +21,7 @@ st.write(f"Model {accuracy}")
 st.header("Real-Time Prediction")
 input_data = []
 for col in X_test.columns:
-    input_value = st.numner_input(f'Input for feature {col}' , value='')
+    input_value = st.numner_input(f'Input for feature {col}' , value='0.0')
     input_data.append(input_value)
     #convert input data to dataframe
     input_df = pd.DataFrame([input_data], columns=X_test.columns)
